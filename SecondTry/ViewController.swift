a41d27033aa72664693560a8ad8a931b1c348578
Chengcheng LIU <lcc@chengchengs-macbook-pro.local>
2017-04-13 17:54:15 +1000

    Initial Commit

---
+++ SecondTry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondTry
+//
+//  Created by Chengcheng on 13/4/17.
+//  Copyright © 2017 Chengcheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
